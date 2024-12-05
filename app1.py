cac8d0b0e9cb0cfdf2049f8f1df69b0e84c42f14
Tri Anggoro <70032017+xxnxxxx@users.noreply.github.com>
2024-12-05 23:18:11 +0700

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import joblib
+import pandas as pd
+import numpy as np
+from sklearn.metrics import classification_report
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Memuat model SVM dan TF-IDF Vectorizer yang sudah disimpan
+svm_model = joblib.load('svm_model.pkl')
+vectorizer = joblib.load('vectorizer.pkl')
+
+# Menampilkan judul aplikasi
+st.title("Prediksi Kategori Teks dengan SVM ")
+
+# Membuat input teks untuk prediksi
+input_text = st.text_area("Masukkan teks untuk prediksi", "")
+
+# Menambahkan tombol untuk memprediksi
+if st.button("Prediksi"):
+    if input_text:
+        # Melakukan transformasi TF-IDF pada input teks
+        input_vector = vectorizer.transform([input_text])
+
+        # Melakukan prediksi
+        prediction = svm_model.predict(input_vector)
+
+        # Menampilkan hasil prediksi
+        st.write(f"Prediksi Kategori: {prediction[0]}")
+
+    else:
+        st.warning("Silakan masukkan teks untuk prediksi.")
